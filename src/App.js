ecb1a3c1384d8aecd103ac48c721797a45f1e48a
Daniel Avila <iamdanielavilavera@gmail.com>
2022-10-14 18:31:47 -0500

    geo

--- src/App.js
+++ src/App.js
@@ -5,24 +5,37 @@ import { useGeoLocation } from './useGeoLocation';
 
 function App() {
 
+  const [loadingLocation, setLoadingLocation] = useState(true)
+
   const [list, setList] = useState([]);
 
-  const { latitude, longitude, locerror } = useGeoLocation()
+  const { latitude, longitude, locerror } = useGeoLocation(true)
 
   useEffect(() => {
     console.log('latitude', latitude);
     console.log('longitude', longitude);
-    setList([
-      ...list,
-      {
-        latitude,
-        longitude,
-      },
-    ]);
+    
+    if(latitude && longitude){
+      setLoadingLocation(false)
+      setList([
+        ...list,
+        {
+          latitude,
+          longitude,
+        },
+      ]);
+    }
   }, [latitude]);
 
   return (
     <div className="App">
+
+{ 
+
+loadingLocation && <div>Obteniendo coordenadas</div>
+
+}
+
       {list.map((location, i) => {
         return (
           <div key={i}>
